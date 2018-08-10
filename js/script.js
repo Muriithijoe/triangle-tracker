906948fa262058edf5de403628306b1d8a92f0f6
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-08-10 13:20:50 +0300

    script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var a =parseInt(prompt("Enter the side A"));
+var b =parseInt(prompt("Enter the side B"));
+var c =parseInt(prompt("Enter the side C"));
+
+//conditions to be met
+if(a===b && b===c){
+alert("This is an equilateral triangle");
+}
+else if(a===b || b===c || a===c){
+alert("This is an isosceles triangle");
+}
+else if(a+b>c||b+c>c||a+c>b){
+alert("This is a scalene triangle");
+}
+else if(a+b<=c||b+c<=a||a+c<=b){
+alert("This is not a triangle");
+}
+else{
+alert("TRY AGAIN.WRONG INPUT");
+}
